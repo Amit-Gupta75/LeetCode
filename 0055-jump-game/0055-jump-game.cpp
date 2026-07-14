07d95c0abe2ce853ddc68c70f1eed523fbaa3800
Amit gupta <guptaamit7571@gmail.com>
2026-07-14 23:11:57 +0530

    Time: 595 ms (7.45%), Space: 55.8 MB (6.37%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool solve(vector<int>& nums, int index, vector<int>& dp) {
+        int n = nums.size();
+
+        
+        if (index >= n - 1)
+            return true;
+
+        
+        if (dp[index] != -1)
+            return dp[index];
+
+        
+        for (int jump = 1; jump <= nums[index]; jump++) {
+            if (solve(nums, index + jump, dp))
+                return dp[index] = true;
+        }
+
+        return dp[index] = false;
+    }
+
+    bool canJump(vector<int>& nums) {
+        vector<int> dp(nums.size(), -1);   
+        return solve(nums, 0, dp);
+    }
+};
